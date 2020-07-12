81b1e946c56d1ecf908af95bf7aa00a55da9262f
Rodrigo Pinheiro Marques de Araújo <fenrrir@gmail.com>
2020-07-11 23:52:54 -0300

    bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='dokkupy',
-    version='0.2',
+    version='0.2.1',
     description='Python API and script for dokku',
     long_description=long_description,
     url='https://github.com/fenrrir/dokkupy',
